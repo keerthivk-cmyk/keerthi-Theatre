2c64adde870b9f5dbfada273a5c1d8da86bfca66
keerthiduk <keerthi.vk@duk.ac.in>
2025-11-15 11:58:05 +0530

    updated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import Welcome from "./Welcome";
 import MovieInfo from "./MovieInfo";
 import { JSX } from "react";
+
+import "./App.css";
+import "./Header.css";
+
 function App(): JSX.Element {
 return (
-<div>
-    <h1>Kerala Theatre Portal</h1>
-    <Welcome name="CDIPD Team" />
+<div className="app-container">
+    <header>
+        <h1>Kerala Theatre Portal</h1>
+    </header>
+    <div className="welcome-section">
+        <Welcome name="CDIPD Team" />
+    </div>
+
         <MovieInfo title="Thenmavin kombath" language="Malayalam" rating={4.9} />
+
+        <footer className="footer">
+            © 2025 Kerala Theatre Portal | Created by Keerthi
+        </footer>
 </div>
 );
 }
--- src/MovieInfo.tsx
+++ src/MovieInfo.tsx
@@ -1,3 +1,4 @@
+import "./MovieInfo.css"
 import { JSX } from "react";
 
 interface MovieProps {
@@ -8,10 +9,10 @@ rating: number;
 export default function MovieInfo({ title, language, rating }:
     MovieProps): JSX.Element {
 return (
-<div>
-<h3>{title}</h3>
-<p>{language}</p>
-<p>⭐ Rating: {rating}/5</p>
+<div className="movie-card">
+<h3 className="movie-title">{title}</h3>
+<p className="movie-language">{language}</p>
+<p className="movie-rating">⭐ Rating: {rating}/5</p>
 </div>
 );
     }
